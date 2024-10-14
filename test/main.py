90c478809f01fcaab1953741baf2e57618550e5c
Ivan <vany.kozlov5@mail.ru>
2024-10-14 14:17:01 +0500

    new rcnn

--- test/main.py
+++ test/main.py
@@ -1,16 +1,12 @@
 import collections
 import string
-import time
 import numpy as np
-import pandas as pd
 from keras.preprocessing.text import Tokenizer
 from keras.preprocessing.sequence import pad_sequences
-from keras.models import Sequential 
-from keras.layers import Dense, LSTM, Embedding, RepeatVector, Dropout
-from keras.layers import GRU, Input, Dense, TimeDistributed, Activation, RepeatVector, Bidirectional
+from keras.models import Model
+from keras.layers import GRU, Input, Dense, TimeDistributed, Activation, RepeatVector, Bidirectional, Embedding, LSTM
 # from keras.layers.embeddings import Embedding
 from keras.optimizers import Adam
-from keras.models import load_model 
 from keras.losses import sparse_categorical_crossentropy
 
 def read_text(filename):
@@ -33,16 +29,22 @@ deu_eng[:,1] = [s.translate(str.maketrans('', '', string.punctuation)) for s in 
 for i in range(len(deu_eng)): 
     deu_eng[i,0] = deu_eng[i,0].lower() 
     deu_eng[i,1] = deu_eng[i,1].lower()
-# print('{} la words.'.format(len([word for sentence in la_words_counter for word in sentence.split()])))
-# print('{} unique la words.'.format(len(la_words_counter)))
-# print('10 Most common words in the la dataset:')
-# print('"' + '" "'.join(list(zip(*la_words_counter.most_common(10)))[0]) + '"')
-# print()
-# print('{} en words.'.format(len([word for sentence in en_words_counter for word in sentence.split()])))
-# print('{} unique en words.'.format(len(en_words_counter)))
-# print('10 Most common words in the en dataset:')
-# print('"' + '" "'.join(list(zip(*en_words_counter.most_common(10)))[0]) + '"')
 
+for sample_i in range(2):
+    print('small_vocab_en Line {}:  {}'.format(sample_i + 1, deu_eng[sample_i,0]))
+    print('small_vocab_fr Line {}:  {}'.format(sample_i + 1, deu_eng[sample_i,1]))
+
+english_words_counter = collections.Counter([word for sentence in deu_eng[:,0] for word in sentence.split()])
+deutsh_words_counter = collections.Counter([word for sentence in deu_eng[:,1] for word in sentence.split()])
+print('{} English words.'.format(len([word for sentence in deu_eng[:,0] for word in sentence.split()])))
+print('{} unique English words.'.format(len(english_words_counter)))
+print('10 Most common words in the English dataset:')
+print('"' + '" "'.join(list(zip(*english_words_counter.most_common(10)))[0]) + '"')
+print()
+print('{} Deutsh words.'.format(len([word for sentence in deu_eng[:,1] for word in sentence.split()])))
+print('{} unique Deutsh words.'.format(len(deutsh_words_counter)))
+print('10 Most common words in the Deutsh dataset:')
+print('"' + '" "'.join(list(zip(*deutsh_words_counter.most_common(10)))[0]) + '"')
 
 def tokenize(x):
     """
@@ -51,10 +53,9 @@ def tokenize(x):
     :return: Tuple of (tokenized x data, tokenizer used to tokenize x)
     """
     # TODO: Implement
-    tokenizer = Tokenizer()
-    tokenizer.fit_on_texts(x)
-    return tokenizer.texts_to_sequences(x), tokenizer
-
+    x_tk = Tokenizer()
+    x_tk.fit_on_texts(x)
+    return x_tk.texts_to_sequences(x), x_tk
 
 def pad(x, length=None):
     """
@@ -63,17 +64,10 @@ def pad(x, length=None):
     :param length: Length to pad the sequence to.  If None, use length of longest sequence in x.
     :return: Padded numpy array of sequences
     """
+    # TODO: Implement
     if length is None:
         length = max([len(sentence) for sentence in x])
     return pad_sequences(x, maxlen=length, padding='post', truncating='post')
-
-# Pad Tokenized output
-# test_pad = pad(text_tokenized)
-# for sample_i, (token_sent, pad_sent) in enumerate(zip(text_tokenized, test_pad)):
-#     print('Sequence {} in x'.format(sample_i + 1))
-#     print('  Input:  {}'.format(np.array(token_sent)))
-#     print('  Output: {}'.format(pad_sent))
-
 
 def preprocess(x, y):
     """
@@ -90,22 +84,13 @@ def preprocess(x, y):
     preprocess_y = preprocess_y.reshape(*preprocess_y.shape, 1)
     return preprocess_x, preprocess_y, x_tk, y_tk
 
-# preproc_la_sentences, preproc_en_sentences, la_tokenizer, en_tokenizer = preprocess(deu_eng[:,0], deu_eng[:,1])
-preproc_la_sentences, preproc_en_sentences, la_tokenizer, en_tokenizer = preprocess(["The buzzer sounded", "The clock is wrong", "The alarm went off"], deu_eng[:,1])
+preproc_english_sentences, preproc_deutsh_sentences, english_tokenizer, deutsh_tokenizer = preprocess(deu_eng[:,0], deu_eng[:,1])
+    
+max_english_sequence_length = preproc_english_sentences.shape[1]
+max_deutsh_sequence_length = preproc_deutsh_sentences.shape[1]
+english_vocab_size = len(english_tokenizer.word_index)
+deutsh_vocab_size = len(deutsh_tokenizer.word_index)
 
-print(deu_eng[:1,0])
-print(deu_eng[:1,1])
-
-
-max_la_sequence_length = preproc_la_sentences.shape[1]
-max_en_sequence_length = preproc_en_sentences.shape[1]
-la_vocab_size = len(la_tokenizer.word_index)
-en_vocab_size = len(en_tokenizer.word_index)
-print('Data Preprocessed')
-print("Max la sentence length:", max_la_sequence_length)
-print("Max en sentence length:", max_en_sequence_length)
-print("Latin vocabulary size:", la_vocab_size)
-print("English vocabulary size:", en_vocab_size)
 
 def logits_to_text(logits, tokenizer):
     """
@@ -118,52 +103,44 @@ def logits_to_text(logits, tokenizer):
     index_to_words[0] = '<PAD>'
     return ' '.join([index_to_words[prediction] for prediction in np.argmax(logits, 1)])
 
-def simple_model(input_shape, output_sequence_length, english_vocab_size, french_vocab_size):
-    """
-    Build and train a basic RNN on x and y
-    :param input_shape: Tuple of input shape
-    :param output_sequence_length: Length of output sequence
-    :param english_vocab_size: Number of unique English words in the dataset
-    :param french_vocab_size: Number of unique French words in the dataset
-    :return: Keras model built, but not trained
-    """
-    # TODO: Build the layers
-    learning_rate = 1e-3
-    model = Sequential()
-    model.add(GRU(128, input_shape=input_shape[1:], return_sequences=True))
-    model.add(Dropout(0.5))
-    model.add(GRU(128, return_sequences=True))
-    model.add(Dropout(0.5))
-    model.add(TimeDistributed(Dense(256, activation='relu')))
-    model.add(Dropout(0.5))
-    model.add(TimeDistributed(Dense(french_vocab_size, activation='softmax'))) 
+def model_final(input_shape, output_sequence_length, english_vocab_size, french_vocab_size):
+    learning_rate = 0.001
+    inputs = Input(shape=input_shape[1:])
+    emb = Embedding(english_vocab_size, 100)(inputs)
+    gru = Bidirectional(GRU(128, dropout=0.5))(emb)
+    final_enc = Dense(256, activation='relu')(gru)
+    
+    dec1 = RepeatVector(output_sequence_length)(final_enc)
+    decgru = Bidirectional(LSTM(512, dropout=0.2, return_sequences=True))(dec1)
+    layer = TimeDistributed(Dense(french_vocab_size, activation='softmax'))
+    final = layer(decgru)
     
     
+    model = Model(inputs=inputs, outputs=final)
     model.compile(loss=sparse_categorical_crossentropy,
                   optimizer=Adam(learning_rate),
                   metrics=['accuracy'])
     return model
+    
+ 
+model = model_final(preproc_english_sentences.shape, preproc_deutsh_sentences.shape[1],len(english_tokenizer.word_index)+1, len(deutsh_tokenizer.word_index)+1)
+model.fit(preproc_english_sentences, preproc_deutsh_sentences, batch_size=300, epochs=30, validation_split=0.2)
 
-# Reshaping the input to work with a basic RNN
+model.save('model.h5')
 
+y_id_to_word = {value: key for key, value in deutsh_tokenizer.word_index.items()}
 
-tmp_x = pad(preproc_la_sentences, max_la_sequence_length)
-print(tmp_x)
-tmp_x = tmp_x.reshape((-1, preproc_en_sentences.shape[-2], 1))
+y_id_to_word[0] = '<PAD>'
+sentence = 'how old are you'
+sentence = [english_tokenizer.word_index[word] for word in sentence.split()]
 
-print(tmp_x[:1])
+sentence = pad_sequences([sentence], maxlen=preproc_english_sentences.shape[-1], padding='post')
+sentences = np.array([sentence[0], preproc_english_sentences[0]])
+predictions = model.predict(sentences, len(sentences))
 
-# # Train the neural network
-# simple_rnn_model = simple_model(
-#     tmp_x.shape,
-#     max_en_sequence_length,
-#     la_vocab_size + 1,
-#     en_vocab_size + 1)
-# simple_rnn_model.fit(tmp_x, preproc_en_sentences, batch_size=300, epochs=10, validation_split=0.2)
-# # Print prediction(s)
-
-# simple_rnn_model.save('model.h5')
-
-model = load_model('model.h5')
-
-print(logits_to_text(model.predict(tmp_x[:1])[0], en_tokenizer))
+print('Sample 1:')
+print(' '.join([y_id_to_word[np.argmax(x)] for x in predictions[0]]))
+print('wie alt sind sie')
+print('Sample 2:')
+print(' '.join([y_id_to_word[np.argmax(x)] for x in predictions[1]]))
+print(' '.join([y_id_to_word[np.max(x)] for x in preproc_deutsh_sentences[0]]))
